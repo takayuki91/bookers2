11250dcf14c9c75a1201a5921e53001c3c7c705d
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-16 13:58:50 +0000

    [Add]Bootstrap install

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   
   resources :books, only: [:new, :create, :index, :show, :destroy]
   
+  resources :users, only: [:show, :edit]
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
